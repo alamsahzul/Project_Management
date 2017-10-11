baea190d29f723d8e138f565201ca00dc8017bbb
zul <nasuhahijrah@gmail.com>
2017-10-11 10:17:46 +0700

    done

--- routes/projects.js
+++ routes/projects.js
@@ -29,10 +29,7 @@ module.exports = function(db){
     db.query(`SELECT * FROM users`, (err, dataUser) => {
       let item = dataUser.rows;
       console.log(item);
-      //db.query(`SELECT role FROM members WHERE user_id = $1`, [user_id], (err, data_member) =>{
-        // console.log(user_id);
         res.render('addProject', { title: 'Add Project', page:'Add Project', data: item });
-      //});
     });
   });
 
@@ -55,7 +52,7 @@ module.exports = function(db){
           let member_id = "default";
           db.query(`INSERT INTO members VALUES (${role}, ${project_id}, ${member_id}, ${id_user})`, (err, data, next) => {
             if(err){
-              //console.log("Gagal Menyimpan");
+              return res.send(err);
             }
             //console.log("berhasil menyimpan ke table member");
             res.redirect('/projects');
@@ -64,13 +61,46 @@ module.exports = function(db){
       }); //query project
   }); //router post add
 
+  router.get('/detail/:project_id',  /*loginChecker, */  function(req, res, next){
+    console.log('ini adalah halaman detail project');
+    let project_id = req.params.project_id;
+    console.log(typeof(req.params));
+    console.log(typeof(req.query));
+
+    db.query(`SELECT * FROM projects AS pr JOIN members AS mem
+              ON pr.project_id = mem.project_id JOIN users
+              ON mem.user_id = users.user_id
+              WHERE pr.project_id = '1507608167548'`, (err, data_project) => {
+    //  console.log(data_project.rows);
+      let panjang = data_project.rows.length;
+      //console.log(panjang);
+      res.render('detailProject', { title: 'Detail Project', page:'Detail Project', data: data_project.rows, panjang:panjang });
+    });
+  });
+
+  router.get('/detail/:project_id',  /*loginChecker, */  function(req, res, next){
+    console.log('ini adalah halaman detail project');
+    let project_id = req.params.project_id;
+    console.log(project_id);
+
+    db.query(`SELECT * FROM projects AS pr JOIN members AS mem
+              ON pr.project_id = mem.project_id JOIN users
+              ON mem.user_id = users.user_id
+              WHERE pr.project_id = '1507608167548'`, (err, data_project) => {
+    //  console.log(data_project.rows);
+      let panjang = data_project.rows.length;
+      //console.log(panjang);
+      res.render('detailProject', { title: 'Detail Project', page:'Detail Project', data: data_project.rows, panjang:panjang });
+    });
+  });
+
   router.get('/edit/:project_id',  /*loginChecker, */  function(req, res, next){
     console.log('ini adalah halaman edit project');
     let project_id = req.params.project_id;
     console.log(project_id);
     db.query(`SELECT * FROM projects WHERE project_id = ${project_id}`, (err, data) => {
       let projects_item = data;
-      console.log(projects_item);
+      //console.log(projects_item);
         //res.render('editProject', { title: 'Edit Project', page:'Edit Project', data: item });
     });
   });
@@ -80,7 +110,7 @@ module.exports = function(db){
     // let project_id = req.params.project_id;
     // console.log(project_id);
 
-    // db.query(`SELECT * FROM projects WHERE project_id = ${project_id}`, (err, dataUser) => {
+    // db.query(`UPDATE projects WHERE project_id = ${project_id}`, (err, dataUser) => {
     //   let item = dataUser.rows;
     //   console.log(item);
     //     res.render('editProject', { title: 'Edit Project', page:'Edit Project', data: item });
@@ -90,8 +120,6 @@ module.exports = function(db){
   router.get('/delete/:project_id',  /*loginChecker, */  function(req, res, next){
     console.log('ini adalah halaman delete project');
     let project_id = +(req.params.project_id);
-    console.log(typeof(project_id));
-
     db.query(`DELETE FROM members WHERE project_id = '${project_id}'`, (err, dataUser, next) => {
       if(err) console.log(err, 'gagal dihapus');
       console.log('berhasil dihapus dari tabel members');
@@ -102,4 +130,4 @@ module.exports = function(db){
   }); //router delete
 
   return router;
-}
+}// exports
