937ef564a7c5b677582e01ab87482f256eab203d
Nuralamsah Zulkarnaim <30795934+alamsahzul@users.noreply.github.com>
2017-10-24 15:40:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Cache-Control', 'no-cache'); 
     next();
 });
